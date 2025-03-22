7bf5641127c8a67e3c6c1cce49228965aa3bcd1d
tuyenxngoc <tuyenn9448@gmail.com>
2025-03-22 17:08:51 +0700

    feat: add Product and SlideProduct components with dynamic data rendering and currency formatting

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,44 +1,45 @@
+import { Link } from 'react-router-dom';
+
 import { Rate } from 'antd';
 import { FaSearch, FaShoppingCart } from 'react-icons/fa';
 
-import './test.css';
+import { formatCurrency } from '~/utils/utils';
 
-function Product() {
+import classNames from 'classnames/bind';
+import styles from './Product.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Product({ data, onAddToCart }) {
     return (
-        <div className="item-product">
-            <div className="hover-mask">
-                <a href="https://shome.vn/bon-cau-mot-khoi-grohe-39316000" className="rounded">
-                    <img
-                        src="https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg"
-                        alt="Bồn cầu một khối Grohe 39316000"
-                        className="img-fluid"
-                    />
+        <div className={cx('wrapper')}>
+            <div className={cx('hover-mask')}>
+                <Link to={data.nameSlug} className="rounded">
+                    <img src={data.image} alt={data.name} className="img-fluid" />
                     <div>
                         <i>
                             <FaSearch />
                         </i>
                     </div>
-                </a>
+                </Link>
             </div>
-            <div className="item-content">
-                <a href="/bon-cau-mot-khoi-grohe-39316000" className="link-none link-black col-clear-fix font-15">
-                    <h3 className="font-15">Bồn cầu một khối Grohe 39316000</h3>
-                </a>
+            <div className={cx('content')}>
+                <Link to={data.nameSlug}>
+                    <h3>{data.name}</h3>
+                </Link>
             </div>
-            <div className="item-base-price">
-                <span className="base-price font-11">21,197,455₫</span>
-                <span className="item-sale-off-ratio font-11">(Tiết kiệm: 20%) </span>
+            <div className={cx('base-price')}>
+                <span className={cx('original-price')}>{formatCurrency(data.price)}</span>
+                <span className={cx('sale-off-ratio')}>(Tiết kiệm: {data.discount}%) </span>
             </div>
-            <div className="item-sale-price">16,957,964₫</div>
-            <div className="item-sale-vote">
-                <div className="row">
-                    <div className="col-12">
-                        <Rate value={5} />
-                        <span>(5)</span>
-                        <i>
-                            <FaShoppingCart />
-                        </i>
-                    </div>
+            <div className={cx('sale-price')}>{formatCurrency(data.salePrice)}</div>
+            <div className={cx('actions')}>
+                <div className={cx('vote')}>
+                    <Rate value={data.rating} disabled />
+                    <span>({data.rating})</span>
+                </div>
+                <div className={cx('cart')} onClick={onAddToCart}>
+                    <FaShoppingCart />
                 </div>
             </div>
         </div>
--- src/components/SlideProduct/SlideProduct.js
+++ src/components/SlideProduct/SlideProduct.js
@@ -1,22 +1,70 @@
 import Product from '../Product';
 
+const products = [
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+];
+
 function SlideProduct() {
+    const handleAddToCart = () => {};
+
     return (
         <>
             <div className="container">
                 <div className="row">
-                    <div className="col-3">
-                        <Product />
-                    </div>
-                    <div className="col-3">
-                        <Product />
-                    </div>
-                    <div className="col-3">
-                        <Product />
-                    </div>
-                    <div className="col-3">
-                        <Product />
-                    </div>
+                    {products.map((product, index) => (
+                        <div key={index} className="col">
+                            <Product data={product} onAddToCart={handleAddToCart} />
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
