69bfb80714aa5d5545e61b24a8c8157d34b4dd44
tuyenxngoc <tuyenn9448@gmail.com>
2025-03-23 20:52:29 +0700

    feat: update Product and SlideProduct styles, refactor category data structure in Home component

--- src/components/SlideProduct/SlideProduct.js
+++ src/components/SlideProduct/SlideProduct.js
@@ -1,5 +1,18 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Grid } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/grid';
+
 import Swal from 'sweetalert2';
 import Product from '../Product';
+import { FaCaretRight } from 'react-icons/fa';
+
+import classNames from 'classnames/bind';
+import styles from './SlideProduct.module.scss';
+
+const cx = classNames.bind(styles);
 
 const products = [
     {
@@ -52,6 +65,124 @@ const products = [
         rating: 5,
         reviews: 5,
     },
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+    {
+        name: 'Bồn cầu rời GROHE, thân bồn cầu đặt sàn 2 khối Cube Ceramic 3948400H',
+        nameSlug: 'bon-cau-mot-khoi-grohe-39316000',
+        image: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/bon%20cau/Grohe/bon-cau-grohe-39316000_w255_h255_n.jpg',
+        price: 21197455,
+        discount: 20,
+        salePrice: 16957964,
+        rating: 5,
+        reviews: 5,
+    },
+];
+
+const brands = [
+    {
+        name: 'Grohe',
+        url: '/bon-cau-grohe',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/Logo-thuonghieu/Grohe_w0_h0_n.png',
+    },
+    {
+        name: 'American Standard',
+        url: '/bon-cau-american-standard',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/Logo-thuonghieu/AS_w0_h0_n.png',
+    },
+    {
+        name: 'DeMuhler',
+        url: '/bon-cau-demuhler',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/Logo-thuonghieu/DeMuhler_w0_h0_n.png',
+    },
+    {
+        name: 'Inax',
+        url: '/bon-cau-inax',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/Logo-thuonghieu/Inax_w0_h0_n.png',
+    },
+    {
+        name: 'Toto',
+        url: '/bon-cau-toto',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/Logo-thuonghieu/TOTO_w0_h0_n.png',
+    },
 ];
 
 function SlideProduct() {
@@ -65,17 +196,49 @@ function SlideProduct() {
     };
 
     return (
-        <>
-            <div className="container">
+        <div className="container pb-3">
+            <div className={cx('wrapper')}>
+                <div className="row mb-4">
+                    <div className="col col-md-5 col-8">
+                        <h2 className={cx('title-popup')}>Bồn Cầu</h2>
+                    </div>
+
+                    <div className="col col-md-7 col-4 text-end">
+                        {brands.map((brand, index) => (
+                            <a key={index} href={brand.url} className={cx('btn-brand')}>
+                                <img src={brand.img} alt={brand.name} width={100} className="img-fluid" />
+                            </a>
+                        ))}
+                        <a className={cx('btn-brand', 'btn-read-more')} href="/bon-cau">
+                            <span>Xem tất cả</span>
+                            <FaCaretRight />
+                        </a>
+                    </div>
+                </div>
+
                 <div className="row">
-                    {products.map((product, index) => (
-                        <div key={index} className="col">
-                            <Product data={product} onAddToCart={handleAddToCart} />
-                        </div>
-                    ))}
+                    <div className="col col-12">
+                        <Swiper
+                            style={{
+                                '--swiper-navigation-color': '#333',
+                                '--swiper-pagination-color': '#333',
+                            }}
+                            modules={[Navigation, Grid]}
+                            navigation
+                            spaceBetween={10}
+                            slidesPerView={5}
+                            grid={{ rows: 2, fill: 'row' }}
+                        >
+                            {products.map((product, index) => (
+                                <SwiperSlide key={index}>
+                                    <Product data={product} onAddToCart={handleAddToCart} />
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
--- src/pages/User/Home/Home.js
+++ src/pages/User/Home/Home.js
@@ -11,66 +11,64 @@ const cx = classNames.bind(styles);
 
 const categories = [
     {
-        href: '/combo-thiet-bi-ve-sinh-nha-tam',
-        imgSrc: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/combo-thiet-bi-ve-sinh_w143_h143_n.png',
+        nameSlug: '/combo-thiet-bi-ve-sinh-nha-tam',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/combo-thiet-bi-ve-sinh_w143_h143_n.png',
         title: 'Combo Thiết Bị Vệ Sinh',
     },
     {
-        href: '/combo-thiet-bi-nha-bep',
-        imgSrc: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/combo-nha-bep_w143_h143_n.png',
+        nameSlug: '/combo-thiet-bi-nha-bep',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/combo-nha-bep_w143_h143_n.png',
         title: 'Combo Thiết Bị Nhà Bếp',
     },
     {
-        href: '/bon-cau',
-        imgSrc: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/bon-cau_w143_h143_n.png',
+        nameSlug: '/bon-cau',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/bon-cau_w143_h143_n.png',
         title: 'Bồn Cầu',
     },
     {
-        href: '/bon-tam',
-        imgSrc: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/bon-tam_w143_h143_n.png',
+        nameSlug: '/bon-tam',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/bon-tam_w143_h143_n.png',
         title: 'Bồn Tắm',
     },
     {
-        href: '/sen-tam',
-        imgSrc: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/sen-tam_w143_h143_n.png',
+        nameSlug: '/sen-tam',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/sen-tam_w143_h143_n.png',
         title: 'Sen Tắm',
     },
     {
-        href: '/lavabo',
-        imgSrc: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/lavabo_w143_h143_n.png',
+        nameSlug: '/lavabo',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/lavabo_w143_h143_n.png',
         title: 'Lavabo',
     },
     {
-        href: '/voi-lavabo',
-        imgSrc: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/voi-lavabo_w143_h143_n.png',
+        nameSlug: '/voi-lavabo',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/voi-lavabo_w143_h143_n.png',
         title: 'Vòi Lavabo',
-        style: { borderRadius: '0px 0px 0px 15px' },
     },
     {
-        href: '/bep-dien',
-        imgSrc: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/bep-tu_w143_h143_n.png',
+        nameSlug: '/bep-dien',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/bep-tu_w143_h143_n.png',
         title: 'Bếp Điện',
     },
     {
-        href: '/hut-mui',
-        imgSrc: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/hut-mui_w143_h143_n.png',
+        nameSlug: '/hut-mui',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/hut-mui_w143_h143_n.png',
         title: 'Máy Hút Mùi',
     },
     {
-        href: '/may-rua-bat',
-        imgSrc: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/may-rua-bat_w143_h143_n.png',
+        nameSlug: '/may-rua-bat',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/may-rua-bat_w143_h143_n.png',
         title: 'Máy Rửa Bát',
     },
     {
-        href: '/lo-nuong',
-        imgSrc: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/lo-nuong_w143_h143_n.png',
+        nameSlug: '/lo-nuong',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/lo-nuong_w143_h143_n.png',
         title: 'Lò Nướng',
     },
     {
-        href: '/voi-rua-bat',
-        imgSrc: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/voi-rua-bat_w143_h143_n.png',
+        nameSlug: '/voi-rua-bat',
+        img: 'https://shome.vn/Thumb/Web/Resources/Uploaded/18/images/icon-danh-muc/voi-rua-bat_w143_h143_n.png',
         title: 'Vòi Rửa Bát',
-        style: { borderRadius: '0px 0px 15px' },
     },
 ];
 
@@ -93,14 +91,18 @@ function Home() {
                     <div className="row g-0">
                         {categories.map((item, index) => (
                             <div key={index} className="col-md-2 col-3">
-                                <a href={item.href} className={cx('popular-item')}>
-                                    <img src={item.imgSrc} alt={item.title} className="img-fluid" />
-                                    <p className="link-none link-black">{item.title}</p>
+                                <a href={item.nameSlug} className={cx('popular-item')}>
+                                    <img src={item.img} alt={item.title} className="img-fluid" />
+                                    <p>{item.title}</p>
                                 </a>
                             </div>
                         ))}
                     </div>
                 </div>
+
+                <SlideProduct />
+
+                <SlideProduct />
 
                 <SlideProduct />
             </div>
