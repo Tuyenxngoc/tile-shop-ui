d154b3123ab1f76e5d8e3bf1e9141a90a8e71de2
tuyenxngoc <tuyenn9448@gmail.com>
2025-04-14 23:16:54 +0700

    feat: implement User management page with user listing, search, and CRUD functionalities

--- src/pages/Admin/User/User.js
+++ src/pages/Admin/User/User.js
@@ -1,5 +1,263 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Alert, Button, Flex, Input, message, Popconfirm, Select, Space, Switch, Table, Tag } from 'antd';
+import { MdOutlineModeEdit } from 'react-icons/md';
+import { FaRegTrashAlt } from 'react-icons/fa';
+
+import queryString from 'query-string';
+
+import { INITIAL_FILTERS, INITIAL_META } from '~/constants';
+import { deleteUser, getUsers, toggleUserActive } from '~/services/userService';
+
+const options = [
+    { value: 'username', label: 'Tên đăng nhập' },
+    { value: 'email', label: 'Địa chỉ email' },
+    { value: 'fullName', label: 'Họ tên' },
+    { value: 'phoneNumber', label: 'Số điện thoại' },
+];
+
+const genderTagMap = {
+    MALE: <Tag color="blue">Nam</Tag>,
+    FEMALE: <Tag color="pink">Nữ</Tag>,
+    OTHER: <Tag color="gray">Khác</Tag>,
+};
+
 function User() {
-    return <>User</>;
+    const navigate = useNavigate();
+
+    const [meta, setMeta] = useState(INITIAL_META);
+    const [filters, setFilters] = useState(INITIAL_FILTERS);
+
+    const [entityData, setEntityData] = useState(null);
+
+    const [searchInput, setSearchInput] = useState('');
+    const [activeFilterOption, setActiveFilterOption] = useState(options[0].value);
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const handleChangePage = (newPage) => {
+        setFilters((prev) => ({ ...prev, pageNum: newPage }));
+    };
+
+    const handleChangeRowsPerPage = (current, size) => {
+        setFilters((prev) => ({
+            ...prev,
+            pageNum: 1,
+            pageSize: size,
+        }));
+    };
+
+    const handleSortChange = (pagination, filters, sorter) => {
+        const sortOrder = sorter.order === 'ascend' ? true : sorter.order === 'descend' ? false : undefined;
+        setFilters((prev) => ({
+            ...prev,
+            sortBy: sorter.field,
+            isAscending: sortOrder,
+        }));
+    };
+
+    const handleSearch = (searchBy, keyword) => {
+        setFilters((prev) => ({
+            ...prev,
+            pageNum: 1,
+            searchBy: searchBy || activeFilterOption,
+            keyword: keyword || searchInput,
+        }));
+    };
+
+    const handleDeleteEntity = async (id) => {
+        try {
+            const response = await deleteUser(id);
+            if (response.status === 200) {
+                setEntityData((prev) => prev.filter((a) => a.id !== id));
+
+                messageApi.success(response.data.data.message);
+            }
+        } catch (error) {
+            const errorMessage = error.response?.data?.message || 'Có lỗi xảy ra khi xóa.';
+            messageApi.error(errorMessage);
+        }
+    };
+
+    const handleToggleActiveFlag = async (checked, record) => {
+        try {
+            const response = await toggleUserActive(record.id);
+            if (response.status === 200) {
+                const { data, message } = response.data.data;
+                setEntityData((prevData) =>
+                    prevData.map((item) => (item.id === record.id ? { ...item, activeFlag: data } : item)),
+                );
+                messageApi.success(message);
+            }
+        } catch (error) {
+            const errorMessage = error.response?.data?.message || 'Có lỗi xảy ra khi cập nhật.';
+            messageApi.error(errorMessage);
+        }
+    };
+
+    useEffect(() => {
+        const fetchEntities = async () => {
+            setIsLoading(true);
+            setErrorMessage(null);
+            try {
+                const params = queryString.stringify(filters);
+                const response = await getUsers(params);
+                const { meta, items } = response.data.data;
+                setEntityData(items);
+                setMeta(meta);
+            } catch (error) {
+                const errorMessage =
+                    error.response?.data?.message || error.message || 'Đã có lỗi xảy ra, vui lòng thử lại sau.';
+                setErrorMessage(errorMessage);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchEntities();
+    }, [filters]);
+
+    const columns = [
+        {
+            title: 'Tên đăng nhập',
+            dataIndex: 'username',
+            key: 'username',
+            sorter: true,
+            showSorterTooltip: false,
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email',
+            sorter: true,
+            showSorterTooltip: false,
+        },
+        {
+            title: 'Số điện thoại',
+            dataIndex: 'phoneNumber',
+            key: 'phoneNumber',
+            sorter: true,
+            showSorterTooltip: false,
+        },
+        {
+            title: 'Họ và tên',
+            dataIndex: 'fullName',
+            key: 'fullName',
+            sorter: true,
+            showSorterTooltip: false,
+        },
+        {
+            title: 'Địa chỉ',
+            dataIndex: 'address',
+            key: 'address',
+            sorter: true,
+            showSorterTooltip: false,
+        },
+        {
+            title: 'Giới tính',
+            dataIndex: 'gender',
+            key: 'gender',
+            sorter: true,
+            showSorterTooltip: false,
+            render: (gender) => genderTagMap[gender] || null,
+        },
+        {
+            title: 'Vai trò',
+            dataIndex: ['role', 'name'],
+            key: 'role',
+            sorter: true,
+            showSorterTooltip: false,
+        },
+        {
+            title: 'Trạng thái',
+            dataIndex: 'activeFlag',
+            key: 'activeFlag',
+            render: (text, record) => (
+                <Space>
+                    {text ? 'Đang hoạt động' : 'Ngừng hoạt động'}
+                    <Switch checked={text} onChange={(checked) => handleToggleActiveFlag(checked, record)} />
+                </Space>
+            ),
+        },
+        {
+            title: 'Thao tác',
+            key: 'action',
+            render: (_, record) => (
+                <Space>
+                    <Button type="text" icon={<MdOutlineModeEdit />} onClick={() => navigate(`edit/${record.id}`)} />
+                    <Popconfirm
+                        title="Thông báo"
+                        description={'Bạn có chắc muốn xóa người dùng này không?'}
+                        onConfirm={() => handleDeleteEntity(record.id)}
+                        okText="Xóa"
+                        cancelText="Hủy"
+                    >
+                        <Button type="text" danger icon={<FaRegTrashAlt />} />
+                    </Popconfirm>
+                </Space>
+            ),
+        },
+    ];
+
+    if (errorMessage) {
+        return <Alert message="Lỗi" description={errorMessage} type="error" />;
+    }
+
+    return (
+        <div>
+            {contextHolder}
+
+            <Flex wrap justify="space-between" align="center">
+                <h2>Quản lý người dùng</h2>
+                <Space>
+                    <Space.Compact className="my-2">
+                        <Select
+                            options={options}
+                            disabled={isLoading}
+                            value={activeFilterOption}
+                            onChange={(value) => setActiveFilterOption(value)}
+                        />
+                        <Input
+                            allowClear
+                            name="searchInput"
+                            placeholder="Nhập từ cần tìm..."
+                            value={searchInput}
+                            disabled={isLoading}
+                            onChange={(e) => setSearchInput(e.target.value)}
+                        />
+                        <Button type="primary" loading={isLoading} onClick={() => handleSearch()}>
+                            Tìm
+                        </Button>
+                    </Space.Compact>
+
+                    <Button type="primary" onClick={() => navigate('new')}>
+                        Thêm mới
+                    </Button>
+                </Space>
+            </Flex>
+
+            <Table
+                bordered
+                rowKey="id"
+                scroll={{ x: 'max-content' }}
+                dataSource={entityData}
+                columns={columns}
+                loading={isLoading}
+                onChange={handleSortChange}
+                pagination={{
+                    current: filters.pageNum,
+                    pageSize: filters.pageSize,
+                    total: meta.totalElements,
+                    onChange: handleChangePage,
+                    showSizeChanger: true,
+                    onShowSizeChange: handleChangeRowsPerPage,
+                }}
+            />
+        </div>
+    );
 }
 
 export default User;
---
+++ src/services/userService.js
@@ -0,0 +1,25 @@
+import { axiosPrivate } from '~/utils/httpRequest';
+
+export const getUserById = (id) => {
+    return axiosPrivate.get(`users/${id}`);
+};
+
+export const getUsers = (params) => {
+    return axiosPrivate.get(`users?${params}`);
+};
+
+export const updateUser = (id, values) => {
+    return axiosPrivate.put(`users/${id}`, values);
+};
+
+export const createUser = (values) => {
+    return axiosPrivate.post('users', values);
+};
+
+export const deleteUser = (id) => {
+    return axiosPrivate.delete(`users/${id}`);
+};
+
+export const toggleUserActive = (id) => {
+    return axiosPrivate.patch(`users/${id}/toggle-active`);
+};
