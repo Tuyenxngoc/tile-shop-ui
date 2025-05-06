5c5e90dc55b33fe4abd541fab864da4245afe146
tuyenxngoc <tuyenn9448@gmail.com>
2025-05-06 09:59:46 +0700

    feat: implement PopularCategories component and integrate it into Home page

---
+++ src/components/PopularCategories/PopularCategories.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Alert, Spin } from 'antd';
+
+import classNames from 'classnames/bind';
+import styles from './PopularCategories.module.scss';
+
+import images from '~/assets';
+import { getCategories } from '~/services/categoryService';
+
+const cx = classNames.bind(styles);
+
+function PopularCategories() {
+    const [categories, setCategories] = useState(null);
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    useEffect(() => {
+        const fetchEntities = async () => {
+            setIsLoading(true);
+            setErrorMessage(null);
+            try {
+                const response = await getCategories({ pageNum: 1, pageSize: 12 });
+                const { items } = response.data.data;
+                setCategories(items);
+            } catch (error) {
+                const errorMessage =
+                    error.response?.data?.message || error.message || 'Đã có lỗi xảy ra, vui lòng thử lại sau.';
+                setErrorMessage(errorMessage);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchEntities();
+    }, []);
+
+    return (
+        <div className="row g-0">
+            {isLoading ? (
+                <div className="d-flex justify-content-center w-100">
+                    <Spin size="large" />
+                </div>
+            ) : errorMessage ? (
+                <div className="w-100">
+                    <Alert message="Lỗi" description={errorMessage} type="error" />
+                </div>
+            ) : (
+                categories.map((item, index) => (
+                    <div key={index} className="col-md-2 col-3">
+                        <Link to={item.slug} className={cx('popular-item')}>
+                            <img src={item.imageUrl || images.categoryDefault} alt={item.name} className="img-fluid" />
+                            <p>{item.name}</p>
+                        </Link>
+                    </div>
+                ))
+            )}
+        </div>
+    );
+}
+
+export default PopularCategories;
--- src/pages/User/Home/Home.js
+++ src/pages/User/Home/Home.js
@@ -1,57 +1,26 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Alert, Spin } from 'antd';
 
-import classNames from 'classnames/bind';
-import styles from './Home.module.scss';
-
-import images from '~/assets';
 import useStore from '~/hooks/useStore';
 import SlideProduct from '~/components/SlideProduct';
 import Carousel from '~/components/Carousel';
 import Policy from '~/components/Policy';
 import SlideNew from '~/components/SlideNew';
-import { getCategories } from '~/services/categoryService';
-
-const cx = classNames.bind(styles);
+import PopularCategories from '~/components/PopularCategories';
 
 function Home() {
     const {
         storeInfo: { bannerLink, bannerImage, backgroundImage, backgroundColor },
     } = useStore();
 
-    const [categories, setCategories] = useState(null);
-
-    const [isLoading, setIsLoading] = useState(true);
-    const [errorMessage, setErrorMessage] = useState(null);
-
-    useEffect(() => {
-        const fetchEntities = async () => {
-            setIsLoading(true);
-            setErrorMessage(null);
-            try {
-                const response = await getCategories({ pageNum: 1, pageSize: 12 });
-                const { items } = response.data.data;
-                setCategories(items);
-            } catch (error) {
-                const errorMessage =
-                    error.response?.data?.message || error.message || 'Đã có lỗi xảy ra, vui lòng thử lại sau.';
-                setErrorMessage(errorMessage);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-
-        fetchEntities();
-    }, []);
-
     return (
         <>
             <div
-                className={cx('banner')}
                 style={{
                     backgroundImage: `url(${backgroundImage})`,
                     backgroundColor: backgroundColor,
+                    backgroundSize: '100%',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPositionX: 'center',
                 }}
             >
                 <div className="container pb-3">
@@ -66,30 +35,7 @@ function Home() {
                 <Policy />
 
                 <div className="container pb-3">
-                    <div className="row g-0">
-                        {isLoading ? (
-                            <div className="d-flex justify-content-center w-100">
-                                <Spin size="large" />
-                            </div>
-                        ) : errorMessage ? (
-                            <div className="w-100">
-                                <Alert message="Lỗi" description={errorMessage} type="error" />
-                            </div>
-                        ) : (
-                            categories.map((item, index) => (
-                                <div key={index} className="col-md-2 col-3">
-                                    <Link to={item.slug} className={cx('popular-item')}>
-                                        <img
-                                            src={item.imgUrl || images.categoryDefault}
-                                            alt={item.name}
-                                            className="img-fluid"
-                                        />
-                                        <p>{item.name}</p>
-                                    </Link>
-                                </div>
-                            ))
-                        )}
-                    </div>
+                    <PopularCategories />
                 </div>
 
                 <SlideProduct />
